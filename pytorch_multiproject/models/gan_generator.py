86591d63129c29706b138fa705925d65ef337a97
Maksym Tatariants <userboxj@gmail.com>
2019-10-31 10:25:16 +0200

    conv2d correction

--- pytorch_multiproject/models/gan_generator.py
+++ pytorch_multiproject/models/gan_generator.py
@@ -41,7 +41,7 @@ class GanGenerator(nn.Module):
 
         resblocks = [ResBlock()] * 9
 
-        upsampling = [nn.ConvTranspose2d(256, 128, kernel_size=3, stride=2,padding=1, output_padding=1, bias=True),
+        upsampling = [nn.ConvTranspose2d(256, 128, kernel_size=3, stride=2, padding=1, output_padding=1, bias=True),
                       nn.InstanceNorm2d(128),
                       nn.ReLU(True),
                       nn.ConvTranspose2d(128, 64, kernel_size=3, stride=2, padding=1, output_padding=1, bias=True),
@@ -50,7 +50,7 @@ class GanGenerator(nn.Module):
                       ]
 
         block_last = [nn.ReflectionPad2d(3),
-                      nn.Conv2d(64, 1, kernel_size=7, padding=0),
+                      nn.Conv2d(64, 3, kernel_size=7, padding=0),
                       nn.Tanh()]
 
         pipeline = block_initial + downsampling + resblocks + upsampling + block_last
