7ef2e21ecd320ff0b562cf679129716ab7ff82d7
Maksym Tatariants <userboxj@gmail.com>
2019-10-31 14:59:18 +0200

    hore dataset util correction

--- pytorch_multiproject/utils/gan_horses_dataset.py
+++ pytorch_multiproject/utils/gan_horses_dataset.py
@@ -14,6 +14,7 @@ def get_data():
     url = 'https://people.eecs.berkeley.edu/~taesung_park/CycleGAN/datasets/horse2zebra.zip'
     r = requests.get(url)
     local_file_path = os.path.join(data_root, 'horse2zebra_.zip')
+    os.makedirs(os.path.dirname(local_file_path), exist_ok=True)
     with open(local_file_path, 'wb') as f:
         f.write(r.content)
 
