a97d1c161ff8330344da89fc92ae8e32a26c5f79
Maksym Tatariants <userboxj@gmail.com>
2019-11-04 12:45:20 +0200

    train.py corrected to use dataloaders for train and val phases

--- pytorch_multiproject/projects/CycleGAN/train.py
+++ pytorch_multiproject/projects/CycleGAN/train.py
@@ -3,19 +3,16 @@ import os
 ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(os.path.dirname('__file__'))))
 sys.path.insert(0, ROOT_DIR)
 import logging
-import itertools
-import pandas as pd
 import torch
 import torch.nn as nn
 import torchvision.transforms as transforms
 from torch import optim
+from torch.utils.data import DataLoader
 from models.cycle_GAN import CycleGAN, GanGenerator, GanDiscriminator, GanOptimizer, GanLrScheduler
 from data.cycle_gan_dataset import CycleGanDataset
-
 from trainers.cycle_gan_trainer import CycleGanTrainer
-
 from logger.logger import main_run, default_log_config
-from utils import freeze_unfreeze_model, normal_weights
+from utils import normal_weights
 
 # default configuration file with hyperparameters
 DEFAULT_CONFIG = 'train.json'
@@ -25,25 +22,28 @@ def main(config):
     # create an instance of logger
     logger = logging.getLogger(os.path.basename(__file__))
     resources_dir = os.path.join(ROOT_DIR, 'resources', 'horse2zebra')
-    label_path = os.path.join(ROOT_DIR, 'resources', 'dataset_info.csv')
 
-    sources = os.path.join(resources_dir, 'trainA')
-    targets = os.path.join(resources_dir, 'trainB')
+    train_sources = os.path.join(resources_dir, 'trainA')
+    train_targets = os.path.join(resources_dir, 'trainB')
+    test_sources = os.path.join(resources_dir, 'testA')
+    test_targets = os.path.join(resources_dir, 'testB')
 
     trans_non_aug = transforms.Compose([transforms.ToPILImage(),
                                         transforms.Resize((256, 256)),
-                                        transforms.ToTensor(),
-                                        transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
+                                        transforms.ToTensor()])
 
-    dataset_df = pd.read_csv(label_path)
-    dataset = CycleGanDataset(full_df=dataset_df, root=resources_dir, mode='train',
-                              data_paths=[sources, targets], extensions=(('.jpg'),)*2, transform=trans_non_aug)
+    # get datasets
+    train_dataset = CycleGanDataset(root=resources_dir, data_paths=[train_sources, train_targets],
+                                    extensions=(('.jpg'),)*2, transform=trans_non_aug)
+    test_dataset = CycleGanDataset(root=resources_dir, data_paths=[test_sources, test_targets],
+                                   extensions=(('.jpg'),)*2, transform=trans_non_aug)
 
+    # create dataloaders
+    train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True, num_workers=4)
+    test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False, num_workers=4)
+    dataloaders = {'train': train_loader, 'val': test_loader}
 
-    # create dataloader
-    loader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True, num_workers=2)
-
-    # initialize metrics with very high loss values
+    # initialize metrics with very high loss values so that the first iteration of model always overrides them
     metrics = {
         'loss_gen': 100.0,
         'ab_disc_loss': 100.0,
@@ -53,7 +53,7 @@ def main(config):
     epochs = config['epochs']
 
     # define generator
-    generator = GanGenerator(skip_relu=False)
+    generator = GanGenerator(num_resblocks=6, skip_relu=False)
     # define discriminator
     discriminator = GanDiscriminator()
     # define criteria for losses
@@ -79,7 +79,7 @@ def main(config):
     optimizer = GanOptimizer(optim_gen, optim_disc)
     lr_sched = GanLrScheduler(sched_gen, sched_gen)
 
-    trainer = CycleGanTrainer(dataloader=loader, root=ROOT_DIR, model=model, criterion=None, optimizer=optimizer,
+    trainer = CycleGanTrainer(dataloaders=dataloaders, root=ROOT_DIR, model=model, criterion=None, optimizer=optimizer,
                               scheduler=lr_sched, metrics=metrics, epochs=epochs)
 
     trainer.train()
