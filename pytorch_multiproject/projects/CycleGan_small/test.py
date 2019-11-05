16273caa53d709b862a1040fcd1feecb6f94aa26
Maksym Tatariants <userboxj@gmail.com>
2019-11-05 14:22:15 +0200

    test.py corrected for small GAN

--- pytorch_multiproject/projects/CycleGan_small/test.py
+++ pytorch_multiproject/projects/CycleGan_small/test.py
@@ -8,8 +8,9 @@ import torch.nn as nn
 import torchvision.transforms as transforms
 from torch import optim
 from torch.utils.data import DataLoader
-from models.cycle_GAN import CycleGAN, GanGenerator, GanDiscriminator, GanOptimizer, GanLrScheduler
-from data.cycle_gan_dataset import CycleGanDataset
+from torchvision.datasets import MNIST, SVHN
+from models.cycle_GAN_small import CycleGAN, GanGenerator, GanDiscriminator, GanOptimizer, GanLrScheduler
+from data.cycle_gan_dataset_small import CycleGanDatasetSmall
 from trainers.cycle_gan_trainer import CycleGanTrainer
 from logger.logger import main_run, default_log_config
 
@@ -21,18 +22,17 @@ DEFAULT_CONFIG = 'train.json'
 def main(config, args):
     # create an instance of logger
     logger = logging.getLogger(os.path.basename(__file__))
-    resources_dir = os.path.join(ROOT_DIR, 'resources', 'horse2zebra')
+    resources_dir = os.path.join(ROOT_DIR, 'resources', config['resource_dir'])
 
-    test_sources = os.path.join(resources_dir, 'testA')
-    test_targets = os.path.join(resources_dir, 'testB')
+    mnist_test = MNIST(os.path.join(resources_dir, 'mnist_test'), download=True, train=False)
+    svhn_test = SVHN(os.path.join(resources_dir, 'svhn_test'), download=True, split='test')
 
     trans_non_aug = transforms.Compose([transforms.ToPILImage(),
                                         transforms.Resize((256, 256)),
                                         transforms.ToTensor()])
 
     # get datasets
-    test_dataset = CycleGanDataset(root=resources_dir, data_paths=[test_sources, test_targets],
-                                   extensions=(('.jpg'),)*2, transform=trans_non_aug)
+    test_dataset = CycleGanDatasetSmall(mnist_test, svhn_test, sample_size=100, transform=trans_non_aug)
 
     # create dataloaders
     test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False, num_workers=4)
