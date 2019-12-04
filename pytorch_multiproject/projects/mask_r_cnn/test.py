37f1494b1db9f879b6db793bbf92c410f2866d65
Maksym Tatariants <userboxj@gmail.com>
2019-12-04 13:32:32 +0200

    test script added

---
+++ pytorch_multiproject/projects/mask_r_cnn/test.py
@@ -0,0 +1,64 @@
+import sys
+import os
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(os.path.dirname('__file__'))))
+sys.path.insert(0, ROOT_DIR)
+import logging
+import torch
+from models.mask_r_cnn_model import get_mask_r_cnn
+from data.mask_r_cnn_dataset import PennFudanDataset
+from trainers.mask_r_cnn_trainer import MaskRCNNTrainer
+from logger.logger import main_run, default_log_config
+from utils import collate_fn
+import data.custom_transforms as T
+
+# default configuration file with hyperparameters
+DEFAULT_CONFIG = 'train.json'
+torch.manual_seed(0)
+
+
+def main(config, args):
+
+    # train on the GPU or on the CPU, if a GPU is not available
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+    # create an instance of logger
+    logger = logging.getLogger(os.path.basename(__file__))
+    if args.resource_dir is not None:
+        resources_dir = args.resource_dir
+    else:
+        resources_dir = os.path.join(ROOT_DIR, 'resources', config.get('resource_dir', 'PennFudanPed'))
+
+    images = os.path.join(resources_dir, 'PNGImages')
+    masks = os.path.join(resources_dir, 'PedMasks')
+
+    dataset = PennFudanDataset(root=resources_dir, data_paths=[images, masks], extensions=(('.png'), )*2,
+                               transforms=T.ToTensor())
+
+    # split the dataset in train and test set
+    indices = torch.randperm(len(dataset)).tolist()
+    dataset_test = torch.utils.data.Subset(dataset, indices[-50:])
+
+    # define val dataloader
+    data_loader_test = torch.utils.data.DataLoader(
+        dataset_test, batch_size=1, shuffle=False, num_workers=0,
+        collate_fn=collate_fn)
+
+    model = get_mask_r_cnn(num_classes=2, pretrained=False)
+    # move model to the right device
+    model.to(device)
+
+    # construct an optimizer
+    params = [p for p in model.parameters() if p.requires_grad]
+    optimizer = torch.optim.SGD(params, lr=config.get('lr', 0.005), momentum=0.9, weight_decay=0.0005)
+    # and a learning rate scheduler
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=3, gamma=0.1)
+
+    trainer = MaskRCNNTrainer(dataloaders=dataset_test, root=ROOT_DIR, model=model, criterion=None,
+                              optimizer=optimizer, scheduler=lr_scheduler, metrics={}, epochs=1,
+                              save_dir=args.save_dir, checkpoint=args.checkpoint, change_lr=args.change_lr)
+    trainer.test()
+
+
+if __name__ == '__main__':
+    default_log_config()
+    main_run(main, DEFAULT_CONFIG)
