b692b9ca9ceb9f73ee41ba9cd6068405cade5038
Maksym Tatariants <userboxj@gmail.com>
2019-12-05 14:20:31 +0200

    supressed metric logger from pytorch, added python logging outputs instead

--- pytorch_multiproject/trainers/mask_r_cnn_trainer.py
+++ pytorch_multiproject/trainers/mask_r_cnn_trainer.py
@@ -1,20 +1,22 @@
 import os
 import sys
 import math
-import time
 import logging
 import torch
 import torchvision
 from trainers.generic_trainer import GenericTrainer
-from utils import warmup_lr_scheduler, reduce_dict, MetricLogger, SmoothedValue
-from utils.coco_utils import get_coco_api_from_dataset
-from utils.coco_eval import CocoEvaluator
+from utils import warmup_lr_scheduler
 from torchvision.utils import save_image
+from tqdm import tqdm
+# import time
+# from utils import reduce_dict, MetricLogger, SmoothedValue
+# from utils.coco_utils import get_coco_api_from_dataset
+# from utils.coco_eval import CocoEvaluator
 
 
 class MaskRCNNTrainer(GenericTrainer):
 
-    def __init__(self, dataloaders, *args, **kwargs):
+    def __init__(self, dataloaders, val_phase_freq=1, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         # to supress debug messages from PIL module
@@ -22,6 +24,10 @@ class MaskRCNNTrainer(GenericTrainer):
         self.pil_logger.setLevel(logging.INFO)
         self.logger = logging.getLogger(os.path.basename(__file__))
         self.dataloaders = dataloaders
+
+        assert val_phase_freq != 0, 'val phase frequency must be 1 or higher, or None'
+        self.val_phase_freq = val_phase_freq
+
         # create directory for saving the results of val phase
         self.save_dir_test = os.path.join(self.save_dir, 'mask_r_cnn_test')
         if not os.path.exists(self.save_dir_test):
@@ -33,15 +39,22 @@ class MaskRCNNTrainer(GenericTrainer):
         # print parameters of optimizer and scheduler every epoch
         self.logger.info(str(self.optimizer))
         if self.scheduler is not None:
-            self.logger.info(str(self.scheduler))
+            self.logger.info(str(self.scheduler.state_dict()))
 
         results = {
             'best_performance': False
         }
 
+        self.logger.info('Epoch {}/{}'.format(epoch, self.epochs))
+        self.logger.info('-' * 10)
+
         # execute training and validation
         self.train_one_epoch(epoch)
-        self.val_one_epoch(epoch)
+
+        # execute val phase if conditions are met
+        if (self.val_phase_freq is not None
+                and epoch % self.val_phase_freq == 0):
+            self.val_one_epoch(epoch)
 
         # in the current implementation we save the trained model after each epoch
         results.update({'best_performance': True})
@@ -49,12 +62,16 @@ class MaskRCNNTrainer(GenericTrainer):
         return results
 
     def train_one_epoch(self, epoch):
-        self.model.train()  # Set model to training mode
+
+        self.logger.info('Starting train phase')
+        # print training results every x iterations
+        print_freq = 10
+        self.model.train()
         phase = 'train'
 
-        metric_logger = MetricLogger(delimiter="  ")
-        metric_logger.add_meter('lr', SmoothedValue(window_size=1, fmt='{value:.6f}'))
-        header = 'Epoch: [{}]'.format(epoch)
+        # metric_logger = MetricLogger(delimiter="  ")
+        # metric_logger.add_meter('lr', SmoothedValue(window_size=1, fmt='{value:.6f}'))
+        # header = 'Epoch: [{}]'.format(epoch)
 
         lr_scheduler_warmup = None
         # the first epoch is 1 not 0
@@ -63,7 +80,9 @@ class MaskRCNNTrainer(GenericTrainer):
             warmup_iters = min(1000, len(self.dataloaders[phase]) - 1)
             lr_scheduler_warmup = warmup_lr_scheduler(self.optimizer, warmup_iters, warmup_factor)
 
-        for images, targets in metric_logger.log_every(self.dataloaders[phase], 10, header):
+        t = tqdm(iter(self.dataloaders[phase]), leave=False, total=len(self.dataloaders[phase]))
+        for idx, data in enumerate(t):
+            images, targets = data
             device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
             images = list(image.to(device) for image in images)
@@ -71,16 +90,11 @@ class MaskRCNNTrainer(GenericTrainer):
             loss_dict = self.model(images, targets)
 
             losses = sum(loss for loss in loss_dict.values())
-
-            # reduce losses over all GPUs for logging purposes
-            loss_dict_reduced = reduce_dict(loss_dict)
-            losses_reduced = sum(loss for loss in loss_dict_reduced.values())
-
-            loss_value = losses_reduced.item()
+            loss_value = losses.item()
 
             if not math.isfinite(loss_value):
                 self.logger.info("Loss is {}, stopping training".format(loss_value))
-                self.logger.info(loss_dict_reduced)
+                self.logger.info(loss_dict)
                 sys.exit(1)
 
             self.optimizer.zero_grad()
@@ -90,64 +104,75 @@ class MaskRCNNTrainer(GenericTrainer):
             if lr_scheduler_warmup is not None:
                 lr_scheduler_warmup.step()
 
-            metric_logger.update(loss=losses_reduced, **loss_dict_reduced)
-            metric_logger.update(lr=self.optimizer.param_groups[0]["lr"])
+            if idx % print_freq == 0:
+                print_data = [epoch, self.epochs, idx, len(self.dataloaders[phase]),
+                              self.optimizer.param_groups[0]["lr"], loss_value, losses]
+
+                self.logger.info('Epoch {}/{}  ITER[{}/{}]     optim_lr: {},   loss: {}      {}'.format(*print_data))
+
+            # metric_logger.update(loss=losses_reduced, **loss_dict_reduced)
+            # metric_logger.update(lr=self.optimizer.param_groups[0]["lr"])
 
         self.scheduler.step()
 
     @torch.no_grad()
     def val_one_epoch(self, epoch):
+        self.logger.info('Starting val phase')
+
         self.model.eval()  # Set model to evaluation mode
         phase = 'val'
 
-        n_threads = torch.get_num_threads()
-        torch.set_num_threads(1)
+        # n_threads = torch.get_num_threads()
+        # torch.set_num_threads(1)
         cpu_device = torch.device("cpu")
 
-        metric_logger = MetricLogger(delimiter="  ")
-        header = 'Test:'
+        # metric_logger = MetricLogger(delimiter="  ")
+        # header = 'Test:'
 
-        coco = get_coco_api_from_dataset(self.dataloaders[phase].dataset)
-        iou_types = self._get_iou_types(self.model)
-        coco_evaluator = CocoEvaluator(coco, iou_types)
+        # coco = get_coco_api_from_dataset(self.dataloaders[phase].dataset)
+        # iou_types = self._get_iou_types(self.model)
+        # coco_evaluator = CocoEvaluator(coco, iou_types)
 
-        for image, targets in metric_logger.log_every(self.dataloaders[phase], 100, header):
+        t = tqdm(iter(self.dataloaders[phase]), leave=False, total=len(self.dataloaders[phase]))
+        for image, targets in t:
             device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
             image = list(img.to(device) for img in image)
             targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
 
-            torch.cuda.synchronize()
-            model_time = time.time()
+            # torch.cuda.synchronize()
+            # model_time = time.time()
             outputs = self.model(image)
 
             outputs = [{k: v.to(cpu_device) for k, v in t.items()} for t in outputs]
-            model_time = time.time() - model_time
+            # model_time = time.time() - model_time
 
             res = {target["image_id"].item(): output for target, output in zip(targets, outputs)}
-            evaluator_time = time.time()
-            coco_evaluator.update(res)
-            evaluator_time = time.time() - evaluator_time
-            metric_logger.update(model_time=model_time, evaluator_time=evaluator_time)
+
+            # evaluator_time = time.time()
+            # coco_evaluator.update(res)
+            # evaluator_time = time.time() - evaluator_time
+            # metric_logger.update(model_time=model_time, evaluator_time=evaluator_time)
 
         # gather the stats from all processes
-        metric_logger.synchronize_between_processes()
-        print("Averaged stats:", metric_logger)
-        coco_evaluator.synchronize_between_processes()
+        # metric_logger.synchronize_between_processes()
+        # print("Averaged stats:", metric_logger)
+        # coco_evaluator.synchronize_between_processes()
 
         # accumulate predictions from all images
-        coco_evaluator.accumulate()
-        coco_evaluator.summarize()
-        torch.set_num_threads(n_threads)
-        return coco_evaluator
+        # coco_evaluator.accumulate()
+        # coco_evaluator.summarize()
+        # torch.set_num_threads(n_threads)
 
     @torch.no_grad()
     def test(self, num_masks=5):
         self.model.eval()
-        metric_logger = MetricLogger(delimiter="  ")
-        header = 'Test:'
+
+        # metric_logger = MetricLogger(delimiter="  ")
+        # header = 'Test:'
         cpu_device = torch.device("cpu")
 
-        for idx, data in enumerate(metric_logger.log_every(self.dataloaders, 100, header)):
+        t = tqdm(iter(self.dataloaders), leave=False, total=len(self.dataloaders))
+        for idx, data in enumerate(t):
             device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
             # data[0] - images, data[1] - targets
             image = list(img.to(device) for img in data[0])
