d2058e78a76600bf21607e92a5bbc76f9dd19f1a
Maksym Tatariants <userboxj@gmail.com>
2019-12-09 17:32:48 +0200

    bound box score test added

--- pytorch_multiproject/tests/obj_detection_evaluator_test.py
+++ pytorch_multiproject/tests/obj_detection_evaluator_test.py
@@ -3,9 +3,10 @@ import sys
 ROOT_DIR = os.path.dirname(os.path.abspath(os.path.dirname('__file__')))
 sys.path.append(ROOT_DIR)
 
+import logging
 import numpy as np
-from mock import patch
-from mock import MagicMock
+# from mock import patch
+# from mock import MagicMock
 from utils.detection_evaluator import DetectionEvaluator
 
 
@@ -15,14 +16,32 @@ test_bboxes = {'box_1': [4, 9, 17, 13],
                'box_4': [4, 18, 11, 25],
                'box_5': [2, 5, 6, 10],
                'box_6': [12.5, 2.5, 17.5, 5],
-               'box_7': [16, 4, 20, 7]}
+               'box_7': [16, 4, 20, 7],
+               'box_8': [12, 13, 19, 17]}
 
 # precomputed ious
 iou = {'1_2': 16/99,
        '1_3': 0,
+       '2_8': 28/57,
        '4_3': 10/99,
        '5_1': 6/94,
        '6_7': 5/36}
+
+# targets, predictions
+evaluator_data = [[{'boxes': np.array([test_bboxes['box_1'],
+                                       test_bboxes['box_8']])},
+                   {'boxes': np.array([test_bboxes['box_2'],
+                                      test_bboxes['box_3'],
+                                       test_bboxes['box_5']]), 'scores': np.array([0.99, 0.87, 0.99])}],
+
+                  [{'boxes': np.array([test_bboxes['box_4']])},
+                   {'boxes': np.array([test_bboxes['box_3'],
+                                      test_bboxes['box_2'],
+                                      test_bboxes['box_1']]), 'scores': np.array([0.99, 0.87, 0.2])}],
+
+                  [{'boxes': np.array([test_bboxes['box_6']])},
+                   {'boxes': np.array([test_bboxes['box_7']]), 'scores': np.array([0.99])}]
+                  ]
 
 batched_target = np.array([test_bboxes['box_1'], test_bboxes['box_4'], test_bboxes['box_6']])
 batched_pred = np.array([test_bboxes['box_2'], test_bboxes['box_3'], test_bboxes['box_5'], test_bboxes['box_7']])
@@ -44,3 +63,10 @@ def test_iou():
 def test_iou_batched():
     test_evaluator = DetectionEvaluator()
     assert test_evaluator.batch_iou(targets_predictions_comb[:, :4], targets_predictions_comb[:, 4:]).all() == batched_iou.all()
+
+
+def test_box_score(caplog):
+    test_evaluator = DetectionEvaluator()
+    test_evaluator.data = evaluator_data
+    out = test_evaluator.bbox_score(iou_threshold=0.12, score_threshold=0.6)
+    assert out == (2/6)
