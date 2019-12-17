0d646a1e959d38d1917cef5c5bc4978abc1b5d81
Maksym Tatariants <userboxj@gmail.com>
2019-12-17 15:21:49 +0200

    bbox score test fixed

--- pytorch_multiproject/tests/obj_detection_evaluator_test.py
+++ pytorch_multiproject/tests/obj_detection_evaluator_test.py
@@ -27,25 +27,37 @@ iou = {'1_2': 16/99,
        '5_1': 6/94,
        '6_7': 5/36}
 
+
+TP = np.array([1, 1, 0, 0, 0, 0, 1])
+FP = np.array([0, 0, 1, 1, 1, 1, 0])
+num_targets = 4
+tp_accum = np.cumsum(TP)
+fp_accum = np.cumsum(FP)
+prec_precomp = np.divide(tp_accum, (tp_accum+fp_accum))
+recall_precomp = tp_accum/num_targets
+
 # precomputed average precision
 AP1 = (0.5-0) * 1
-AP2 = (0.75-0.5)*0.3333
+AP2 = (0.75-0.5)*prec_precomp[-1]
 AP3 = (1-0.75)*0
 AP = AP1 + AP2 + AP3
 
 # targets, predictions
-evaluator_data = [[{'boxes': np.array([test_bboxes['box_1'],
+evaluator_data = [[None,
+                   {'boxes': np.array([test_bboxes['box_1'],
                                        test_bboxes['box_8']])},
                    {'boxes': np.array([test_bboxes['box_2'],
                                       test_bboxes['box_3'],
                                        test_bboxes['box_5']]), 'scores': np.array([0.99, 0.87, 0.99])}],
 
-                  [{'boxes': np.array([test_bboxes['box_4']])},
+                  [None,
+                   {'boxes': np.array([test_bboxes['box_4']])},
                    {'boxes': np.array([test_bboxes['box_3'],
                                       test_bboxes['box_2'],
                                       test_bboxes['box_1']]), 'scores': np.array([0.99, 0.87, 0.2])}],
 
-                  [{'boxes': np.array([test_bboxes['box_6']])},
+                  [None,
+                   {'boxes': np.array([test_bboxes['box_6']])},
                    {'boxes': np.array([test_bboxes['box_7']]), 'scores': np.array([0.99])}]
                   ]
 
@@ -58,7 +70,7 @@ targets_predictions_comb = np.hstack([np.repeat(batched_target, batched_pred.sha
 
 
 def test_iou():
-    test_evaluator = DetectionEvaluator()
+    test_evaluator = DetectionEvaluator(save_dir='test')
     assert test_evaluator.intersection_over_union(test_bboxes['box_1'], test_bboxes['box_2']) == iou['1_2']
     assert test_evaluator.intersection_over_union(test_bboxes['box_1'], test_bboxes['box_3']) == iou['1_3']
     assert test_evaluator.intersection_over_union(test_bboxes['box_4'], test_bboxes['box_3']) == iou['4_3']
@@ -67,12 +79,15 @@ def test_iou():
 
 
 def test_iou_batched():
-    test_evaluator = DetectionEvaluator()
+    test_evaluator = DetectionEvaluator(save_dir='test')
     assert test_evaluator.batch_iou(targets_predictions_comb[:, :4], targets_predictions_comb[:, 4:]).all() == batched_iou.all()
 
 
 def test_box_score(caplog):
-    test_evaluator = DetectionEvaluator()
+    test_evaluator = DetectionEvaluator(save_dir='test')
     test_evaluator.data = evaluator_data
-    avg_precision = test_evaluator.bbox_score(iou_threshold=0.12, score_threshold=0.6)
-    assert round(avg_precision, 4) == round(AP, 4)
+    avg_precision, precision, \
+        recall, _ = test_evaluator.bbox_score(iou_threshold=0.12, score_threshold=0.6)
+    assert AP == avg_precision
+    assert prec_precomp[-1] == precision
+    assert recall_precomp[-1] == recall
