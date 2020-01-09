5c889095cfec232e2806d70c80041cd7fb4a8e26
Maksym Tatariants <userboxj@gmail.com>
2020-01-09 14:40:58 +0200

    random crop without resize added

--- pytorch_multiproject/data/custom_transforms.py
+++ pytorch_multiproject/data/custom_transforms.py
@@ -136,6 +136,62 @@ class ResizeBboxImg(object):
         return image, target
 
 
+class RandomCropBbox:
+    """Similar to RandomResizedCropBbox.
+       The ony difference is that RandomCropBbox does not resize the cropped image section and leaves it as is."""
+    def __init__(self, scale=(0.6, 0.9), ratio=(3. / 4., 4. / 3.)):
+        if (scale[0] > scale[1]) or (ratio[0] > ratio[1]):
+            warnings.warn("range should be of kind (min, max)")
+
+        self.scale = scale
+        self.ratio = ratio
+
+    def __call__(self, image, target):
+        """
+        Args:
+            image (PIL Image): Image to be cropped and resized.
+            target (dict): contains masks and coordinates of bounding boxes to be cropped and resized
+
+        Returns:
+            PIL Image: Randomly cropped and resized image.
+        """
+        y, x, h, w = get_crop_params(image, self.scale, self.ratio)
+        image = F.crop(image, y, x, h, w)
+
+        # note that single bbox format is [x0, y0, x1, y1]
+        bbox = target['boxes']
+        # create mask that will drop all bounding boxes that have no intersection with cropped region
+        # the intersection conditions are x0 < x + w, y0 < y + h, x1 > x, y1 > y
+        drop_mask = (bbox[:, 0] < x + w) & (bbox[:, 1] < y + h) & (bbox[:, 2] > x) & (bbox[:, 3] > y)
+        bbox = bbox[drop_mask]
+        # move bonding box coordinates to coordinate system of cropped region
+        bbox[:, 0:3:2] = bbox[:, 0:3:2] - x
+        bbox[:, 1:4:2] = bbox[:, 1:4:2] - y
+        # clamp the bbox coordinate values at the boundaries of cropped region
+        bbox[:, 0:2][bbox[:, 0:2] < 0] = 0
+        bbox[:, 2][bbox[:, 2] > w] = w
+        bbox[:, 3][bbox[:, 3] > h] = h
+        target['boxes'] = bbox
+
+        if 'masks' in target:
+            masks = target['masks']
+            if len(masks.shape) == 3:
+
+                transformed_masks = []
+                for mask in masks:
+                    mask = F.to_pil_image(mask.mul(255))
+                    mask = F.crop(mask, y, x, h, w)
+                    mask = F.to_tensor(mask).squeeze()
+                    mask = mask.to(dtype=torch.uint8)
+                    transformed_masks.append(mask)
+
+                transformed_masks = torch.stack(transformed_masks)
+                target['masks'] = transformed_masks
+            else:
+                raise ValueError('Please provide masks of correct shape: N, H, W')
+        return image, target
+
+
 class RandomResizedCropBbox:
     """ Based on https://pytorch.org/docs/stable/_modules/torchvision/transforms/transforms.html#RandomCrop
     Crop the given PIL Image to random size and aspect ratio.
@@ -164,51 +220,7 @@ class RandomResizedCropBbox:
         self.interpolation = interpolation
         self.scale = scale
         self.ratio = ratio
-    
-    @staticmethod
-    def get_params(image, scale, ratio):
-        """Get parameters for ``crop`` for a random sized crop.
 
-        Args:
-            image (PIL Image): Image to be cropped.
-            scale (tuple): range of size of the origin size cropped
-            ratio (tuple): range of aspect ratio of the origin aspect ratio cropped
-
-        Returns:
-            tuple: params (i, j, h, w) to be passed to ``crop`` for a random
-                sized crop.
-        """
-        width, height = image.size
-        area = height * width
-
-        for attempt in range(10):
-            target_area = random.uniform(*scale) * area
-            log_ratio = (math.log(ratio[0]), math.log(ratio[1]))
-            aspect_ratio = math.exp(random.uniform(*log_ratio))
-
-            w = int(round(math.sqrt(target_area * aspect_ratio)))
-            h = int(round(math.sqrt(target_area / aspect_ratio)))
-
-            if 0 < w <= width and 0 < h <= height:
-                y = random.randint(0, height - h)
-                x = random.randint(0, width - w)
-                return y, x, h, w
-
-        # Fallback to central crop
-        in_ratio = float(width) / float(height)
-        if in_ratio < min(ratio):
-            w = width
-            h = int(round(w / min(ratio)))
-        elif in_ratio > max(ratio):
-            h = height
-            w = int(round(h * max(ratio)))
-        else:  # whole image
-            w = width
-            h = height
-        y = (height - h) // 2
-        x = (width - w) // 2
-        return y, x, h, w
-    
     def __call__(self, image, target):
         """
         Args:
@@ -218,7 +230,7 @@ class RandomResizedCropBbox:
         Returns:
             PIL Image: Randomly cropped and resized image.
         """
-        y, x, h, w = self.get_params(image, self.scale, self.ratio)
+        y, x, h, w = get_crop_params(image, self.scale, self.ratio)
         image = F.resized_crop(image, y, x, h, w, self.size, self.interpolation)
         # get height and width of image after resizing
         w_new, h_new = image.size
@@ -286,3 +298,47 @@ class ToTensor(object):
         image = F.to_tensor(image)
         return image, target
 # end of copied code from https://github.com/pytorch/vision/blob/master/references/detection/transforms.py
+
+
+def get_crop_params(image, scale, ratio):
+    """Get parameters for ``crop`` for a random sized crop.
+
+    Args:
+        image (PIL Image): Image to be cropped.
+        scale (tuple): range of size of the origin size cropped
+        ratio (tuple): range of aspect ratio of the origin aspect ratio cropped
+
+    Returns:
+        tuple: params (i, j, h, w) to be passed to ``crop`` for a random
+            sized crop.
+    """
+    width, height = image.size
+    area = height * width
+
+    for attempt in range(10):
+        target_area = random.uniform(*scale) * area
+        log_ratio = (math.log(ratio[0]), math.log(ratio[1]))
+        aspect_ratio = math.exp(random.uniform(*log_ratio))
+
+        w = int(round(math.sqrt(target_area * aspect_ratio)))
+        h = int(round(math.sqrt(target_area / aspect_ratio)))
+
+        if 0 < w <= width and 0 < h <= height:
+            y = random.randint(0, height - h)
+            x = random.randint(0, width - w)
+            return y, x, h, w
+
+    # Fallback to central crop
+    in_ratio = float(width) / float(height)
+    if in_ratio < min(ratio):
+        w = width
+        h = int(round(w / min(ratio)))
+    elif in_ratio > max(ratio):
+        h = height
+        w = int(round(h * max(ratio)))
+    else:  # whole image
+        w = width
+        h = height
+    y = (height - h) // 2
+    x = (width - w) // 2
+    return y, x, h, w
